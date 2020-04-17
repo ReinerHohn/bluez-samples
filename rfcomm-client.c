6b6b4480b27ecf317d247d176b0600d5fa1da2f0
Michael Dick <mi-dick@gmx.de>
2020-04-17 19:05:52 +0100

    Add reading buffer from file

--- rfcomm-client.c
+++ rfcomm-client.c
@@ -6,7 +6,10 @@
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/rfcomm.h>
 
+#include "common.h"
+
 int s;
+char* readFileInBuffer(char* filename, long* psize);
 
 static void catch_function(int signo)
 {
@@ -20,6 +23,16 @@ int main (int argc, char** argv)
 {
     struct sockaddr_rc addr = { 0 } ;
     int status;
+    long size = 0;
+    long bytes_read = 0;
+    long bytes_write = 0;
+    long read_index = 0;
+    int read_length = 0;
+
+    char buf [BUF_SIZE] = { 0 } ;
+    static const long bufsize = sizeof (buf)/sizeof(char);
+    bytes_write = bufsize;
+    printf(stderr, "bufsize %ld\n");
 
     if ( argc < 2 )
     {
@@ -36,26 +49,54 @@ int main (int argc, char** argv)
         fputs("An error occurred while setting a signal handler.\n", stderr);
         //return EXIT_FAILURE;
     }
+    char* buffer = readFileInBuffer("/opt/hgs/file.txt", &size);
+    if ( buffer )
+    {
+	// allocate a socket
+	s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
-    // allocate a socket
-    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+	// set the connection parameters (who to connect to)
+	addr.rc_family = AF_BLUETOOTH;
+	addr.rc_channel = 1;
+	fprintf(stderr, "%s\n", argv[1]);
+	str2ba(argv[1], &addr.rc_bdaddr);
 
-    // set the connection parameters (who to connect to)
-    addr.rc_family = AF_BLUETOOTH;
-    addr.rc_channel = 1;
-    fprintf(stderr, "%s\n", argv[1]);
-    str2ba(argv[1], &addr.rc_bdaddr);
+	// connect to server
+	status = connect(s, (struct sockaddr*)&addr, sizeof(addr));
 
-    // connect to server
-    status = connect(s, (struct sockaddr*)&addr, sizeof(addr));
+	// send a message
+	if (0 == status) {
+	    status = send(s, "hello!", 6, 0);
+	}
 
-    // send a message
-    if (0 == status) {
-        status = send(s, "hello!", 6, 0);
+	if (status < 0) perror ("uh oh");
+
+	close(s);
+	return 0;
+   }
+   else
+   {
+	   fputs("Could not read /opt/hgs/file.txt\n", stderr);
+   }
+}
+
+char* readFileInBuffer(char* filename, long* psize)
+{
+    char *string = NULL;
+    FILE *f = fopen(filename, "rb");
+    if ( f )
+    {
+        fseek(f, 0, SEEK_END);
+        long fsize = ftell(f);
+        *psize = fsize;
+        fseek(f, 0, SEEK_SET);  /* same as rewind(f); */
+
+        string = malloc(fsize + 1);
+        fread(string, 1, fsize, f);
+        fclose(f);
+
+        string[fsize] = 0;
     }
 
-    if (status < 0) perror ("uh oh");
-
-    close(s);
-    return 0;
+    return string;
 }
