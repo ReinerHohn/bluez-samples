857cd44f0f92fe8d6b4ebfd22e3e069effa00ee7
Michael Dick <mi-dick@gmx.de>
2020-04-21 16:25:14 +0100

    Fix indention

--- rfcomm-client.c
+++ rfcomm-client.c
@@ -16,88 +16,88 @@ char* readFileInBuffer(char* filename, long* psize);
 
 static void catch_function(int signo)
 {
-    puts("Interactive attention signal caught.");
-    close(g_socket);
+	puts("Interactive attention signal caught.");
+	close(g_socket);
 
-    exit(1);
+	exit(1);
 }
 int getl2CapSocket(char* bdaddr, uint16_t mtu)
 {
-    int fd_sock_l2cap;
-    struct sockaddr_l2 addr_l2cap = { 0 };
+	int fd_sock_l2cap;
+	struct sockaddr_l2 addr_l2cap = { 0 };
 
-    fd_sock_l2cap = socket(AF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP);
-    if ( -1 != fd_sock_l2cap )
-    {
-        // set the connection parameters (who to connect to)
-        addr_l2cap.l2_family = AF_BLUETOOTH;
-        addr_l2cap.l2_psm = htobs(0x1001);
-        str2ba( bdaddr, &addr_l2cap.l2_bdaddr );
+	fd_sock_l2cap = socket(AF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP);
+	if ( -1 != fd_sock_l2cap )
+	{
+		// set the connection parameters (who to connect to)
+		addr_l2cap.l2_family = AF_BLUETOOTH;
+		addr_l2cap.l2_psm = htobs(0x1001);
+		str2ba( bdaddr, &addr_l2cap.l2_bdaddr );
 
-        set_l2cap_mtu( fd_sock_l2cap, mtu );
-        // connect to server
-        g_status = connect(fd_sock_l2cap, (struct sockaddr *)&addr_l2cap, sizeof(addr_l2cap));
-    }
-    else
-    {
-        perror("Failed to open socket\n");
-    }
-    return fd_sock_l2cap;
+		set_l2cap_mtu( fd_sock_l2cap, mtu );
+		// connect to server
+		g_status = connect(fd_sock_l2cap, (struct sockaddr *)&addr_l2cap, sizeof(addr_l2cap));
+	}
+	else
+	{
+		perror("Failed to open socket\n");
+	}
+	return fd_sock_l2cap;
 }
 int getRfcommSocket(char* bdaddr)
 {
-    int fd_sock_rfcomm;
-    struct sockaddr_rc addr_rfcomm = { 0 };
+	int fd_sock_rfcomm;
+	struct sockaddr_rc addr_rfcomm = { 0 };
 
-    // allocate a socket
-    fd_sock_rfcomm = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+	// allocate a socket
+	fd_sock_rfcomm = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
-    if ( -1 != fd_sock_rfcomm )
-    {
-        // set the connection parameters (who to connect to)
-        addr_rfcomm.rc_family = AF_BLUETOOTH;
-        addr_rfcomm.rc_channel = 1;
-        fprintf(stderr, "%s\n", bdaddr);
-        str2ba(bdaddr, &addr_rfcomm.rc_bdaddr);
+	if ( -1 != fd_sock_rfcomm )
+	{
+		// set the connection parameters (who to connect to)
+		addr_rfcomm.rc_family = AF_BLUETOOTH;
+		addr_rfcomm.rc_channel = 1;
+		fprintf(stderr, "%s\n", bdaddr);
+		str2ba(bdaddr, &addr_rfcomm.rc_bdaddr);
 
-        // connect to server
-        g_status = connect(fd_sock_rfcomm, (struct sockaddr*)&addr_rfcomm, sizeof(addr_rfcomm));
-    }
-    else
-    {
-        perror("Failed to open socket\n");
-    }
-    return fd_sock_rfcomm;
+		// connect to server
+		g_status = connect(fd_sock_rfcomm, (struct sockaddr*)&addr_rfcomm, sizeof(addr_rfcomm));
+	}
+	else
+	{
+		perror("Failed to open socket\n");
+	}
+	return fd_sock_rfcomm;
 }
 #include <stdio.h>
 
 int sendRecFile(char* buffer_send, char* buffer_recv, long transfer_size, char* filename, long packageSizeSend, long packageSizeRecv, char* protocol, int latency_test)
 {
-    long bytes_read = 0;
-    long write_length = 0;
-    long read_length = 0;
-    long read_index = 0;
-    long write_index = 0;
+	long bytes_read = 0;
+	long write_length = 0;
+	long read_length = 0;
+	long read_index = 0;
+	long write_index = 0;
 
-    struct timeval st, et;
-    struct timeval st_lat, et_lat;
+	struct timeval st, et;
+	struct timeval st_lat, et_lat;
 	double latent_sum = 0;
 	double latent_count = 0;
-    char data[1024];
+	char data[1024];
 
-    FILE* pFile;
-    pFile = fopen(filename, "at");
-    if( pFile )
-    {
-        fprintf(stderr, "Opened file\n");
-    }
-    else
-    {
-        fprintf(stderr, "Failed to open file\n");
-    }
-    write_index = 0;
-    read_index = 0;
-    read_length = packageSizeRecv;
+	FILE* pFile;
+	pFile = fopen(filename, "at");
+	if( pFile )
+	{
+		fprintf(stderr, "Opened file\n");
+	}
+	else
+	{
+		fprintf(stderr, "Failed to open file\n");
+	}
+	write_index = 0;
+	read_index = 0;
+	read_length = packageSizeRecv;
 
 	// In latency test, set transfer size to one packet size!
 	if ( latency_test )
@@ -105,180 +105,180 @@ int sendRecFile(char* buffer_send, char* buffer_recv, long transfer_size, char* 
 		transfer_size = packageSizeSend > packageSizeRecv ? packageSizeSend : packageSizeRecv;
 	}
 
-    gettimeofday(&st,NULL);
-    fprintf(stderr, "Transferring %ld bytes at package size send:%ld, package size recv:%ld\n", transfer_size, packageSizeSend, packageSizeRecv);
-    while( write_index < transfer_size )
-    {
-        if( latency_test ) 
+	gettimeofday(&st,NULL);
+	fprintf(stderr, "Transferring %ld bytes at package size send:%ld, package size recv:%ld\n", transfer_size, packageSizeSend, packageSizeRecv);
+	while( write_index < transfer_size )
 	{
-		gettimeofday(&st_lat,NULL);
-	}
-        write_length = ( transfer_size - write_index) > packageSizeSend ? packageSizeSend : transfer_size - write_index;
-        //fprintf(stderr, "Transferring %ld / %ld \n", writegg_index, transfer_size);
-        g_status = send(g_socket, buffer_send + write_index, write_length, 0);
-        /*gettimeofday(&et,NULL);
-        int elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);*/
-        //double speed = (((double)write_length)/((double)elapsed));
-        //speedsum += speed;
-        //fprintf(stderr, "%d:usec:%ld:bytes, %f MB/s\n",elapsed, write_length, speed);
-        //fprintf(stderr, "%f MB/s av\n",speedsum/counter);
-        //counter++;
-        if (g_status < 0)
-        {
-            perror ("uh oh");
-            break;
-        }
-        write_index += write_length;
+		if( latency_test ) 
+		{
+			gettimeofday(&st_lat,NULL);
+		}
+		write_length = ( transfer_size - write_index) > packageSizeSend ? packageSizeSend : transfer_size - write_index;
+		//fprintf(stderr, "Transferring %ld / %ld \n", writegg_index, transfer_size);
+		g_status = send(g_socket, buffer_send + write_index, write_length, 0);
+		/*gettimeofday(&et,NULL);
+		  int elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);*/
+		//double speed = (((double)write_length)/((double)elapsed));
+		//speedsum += speed;
+		//fprintf(stderr, "%d:usec:%ld:bytes, %f MB/s\n",elapsed, write_length, speed);
+		//fprintf(stderr, "%f MB/s av\n",speedsum/counter);
+		//counter++;
+		if (g_status < 0)
+		{
+			perror ("uh oh");
+			break;
+		}
+		write_index += write_length;
 #if 1//READ_BACK
-        bytes_read = 0;
-        int bytes_read_temp = 0;
-        read_length = ( transfer_size - read_index) > packageSizeRecv ? packageSizeRecv : transfer_size - read_index;
-        while ( bytes_read < read_length )
-        {
-            //fprintf(stderr, "waiting for  %ld bytes \n", read_length - bytes_read);
-            bytes_read_temp = recv(g_socket, buffer_recv + read_index, read_length, 0);
-            //fprintf(stderr, "bytes_read %ld\n", bytes_read_temp);
-            if( bytes_read_temp <= 0 )
-            {
-                if ( read_index == 0 )
-                {
-                return bytes_read_temp;
-                }
-                break;
-                fprintf(stderr, "TIMEOUT\n");
-            }
+		bytes_read = 0;
+		int bytes_read_temp = 0;
+		read_length = ( transfer_size - read_index) > packageSizeRecv ? packageSizeRecv : transfer_size - read_index;
+		while ( bytes_read < read_length )
+		{
+			//fprintf(stderr, "waiting for  %ld bytes \n", read_length - bytes_read);
+			bytes_read_temp = recv(g_socket, buffer_recv + read_index, read_length, 0);
+			//fprintf(stderr, "bytes_read %ld\n", bytes_read_temp);
+			if( bytes_read_temp <= 0 )
+			{
+				if ( read_index == 0 )
+				{
+					return bytes_read_temp;
+				}
+				break;
+				fprintf(stderr, "TIMEOUT\n");
+			}
 
-            read_index += bytes_read_temp;
-            bytes_read += bytes_read_temp;
-        }
-	if( latency_test )
-	{
-		gettimeofday(&et_lat,NULL);
-	        double elapsed = ((et_lat.tv_sec - st_lat.tv_sec) * 1000000) + (et_lat.tv_usec - st_lat.tv_usec);
-		latent_sum += elapsed;
-		latent_count++;
-        //double speed = (((double)write_length)/((double)elapsed));
-        //speedsum += speed;
-        //fprintf(stderr, "%d:usec:%ld:bytes, %f MB/s\n",elapsed, write_length, speed);
-        //fprintf(stderr, "%f MB/s av\n",speedsum/counter);
-        //counter++;
+			read_index += bytes_read_temp;
+			bytes_read += bytes_read_temp;
+		}
+		if( latency_test )
+		{
+			gettimeofday(&et_lat,NULL);
+			double elapsed = ((et_lat.tv_sec - st_lat.tv_sec) * 1000000) + (et_lat.tv_usec - st_lat.tv_usec);
+			latent_sum += elapsed;
+			latent_count++;
+			//double speed = (((double)write_length)/((double)elapsed));
+			//speedsum += speed;
+			//fprintf(stderr, "%d:usec:%ld:bytes, %f MB/s\n",elapsed, write_length, speed);
+			//fprintf(stderr, "%f MB/s av\n",speedsum/counter);
+			//counter++;
 
-	}
+		}
 
-        if ( write_index != read_index )
-        {
-             //fprintf(stderr, "Read write length ummatch write_length: %ld, bytes_read %ld\n", write_length, bytes_read);
-        }
-        //int res =  memcmp(buffer_send + write_index, buffer_recv + read_index, read_length);
-        //if ( res != 0 )
-        {
-            //fprintf(stderr, "Buffer ummatch %d, write_index %d, read_index %d, %s %s \n", res, write_index, read_index, buffer_send + write_index, buffer_recv + read_index);
-            //fprintf(stderr, "Buffer ummatch %d, write_index %d, read_index %d, %s %s \n", res, write_index, read_index, buffer_send + write_index, buffer_recv + read_index);
-        }
-        //printf("received %s\n", buffer_recv);
-        //memset(buffer_recv, 0, bufsize);
+		if ( write_index != read_index )
+		{
+			//fprintf(stderr, "Read write length ummatch write_length: %ld, bytes_read %ld\n", write_length, bytes_read);
+		}
+		//int res =  memcmp(buffer_send + write_index, buffer_recv + read_index, read_length);
+		//if ( res != 0 )
+		{
+			//fprintf(stderr, "Buffer ummatch %d, write_index %d, read_index %d, %s %s \n", res, write_index, read_index, buffer_send + write_index, buffer_recv + read_index);
+			//fprintf(stderr, "Buffer ummatch %d, write_index %d, read_index %d, %s %s \n", res, write_index, read_index, buffer_send + write_index, buffer_recv + read_index);
+		}
+		//printf("received %s\n", buffer_recv);
+		//memset(buffer_recv, 0, bufsize);
 #endif
-    }
-    gettimeofday(&et,NULL);
+	}
+	gettimeofday(&et,NULL);
 	double avg_lat = (latent_sum / latent_count)/(double)1000000;
-    int elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
+	int elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
 	double speed = ((double)transfer_size/(double)elapsed)*1000;
-    sprintf(data, "Trans:%ld: bytes in :%d:s packSRecv:%ld: packSend:%ld: speed:%f: kB/s, latency:%f:,prot:%s:\n", transfer_size, elapsed/ 1000000, packageSizeRecv, packageSizeSend, speed, avg_lat, protocol);
-    fwrite(data, sizeof(char), strlen(data), pFile); //sizeof(data)/sizeof (char)
-    fprintf(stderr, "%s\n", data);
-    fclose(pFile);
-    aligned_free(buffer_recv);
-    aligned_free(buffer_send);
+	sprintf(data, "Trans:%ld: bytes in :%d:s packSRecv:%ld: packSend:%ld: speed:%f: kB/s, latency:%f:,prot:%s:\n", transfer_size, elapsed/ 1000000, packageSizeRecv, packageSizeSend, speed, avg_lat, protocol);
+	fwrite(data, sizeof(char), strlen(data), pFile); //sizeof(data)/sizeof (char)
+	fprintf(stderr, "%s\n", data);
+	fclose(pFile);
+	aligned_free(buffer_recv);
+	aligned_free(buffer_send);
 
 }
 
 int main (int argc, char** argv)
 {
-    long transfer_size = 0;
-    if ( argc < 5 )
-    {
-        printf("Usage: client <bdaddr> <package_size_send> <package_size_recv> <protocol> <latency_test>");
-        return 1;
-    }
-    char filename[50];
-    char* p;
-    long packageSizeSend = strtol(argv[2], &p, 10);
-    long packageSizeRecv = strtol(argv[3], &p, 10);
+	long transfer_size = 0;
+	if ( argc < 5 )
+	{
+		printf("Usage: client <bdaddr> <package_size_send> <package_size_recv> <protocol> <latency_test>");
+		return 1;
+	}
+	char filename[50];
+	char* p;
+	long packageSizeSend = strtol(argv[2], &p, 10);
+	long packageSizeRecv = strtol(argv[3], &p, 10);
 	int latency_test = strtol(argv[5], &p, 10);
 
-    if (signal(SIGINT, catch_function) == SIG_ERR)
-    {
-        fputs("An error occurred while setting a signal handler.\n", stderr);
-        //return EXIT_FAILURE;
-    }
-    if (signal(SIGKILL, catch_function) == SIG_ERR)
-    {
-        fputs("An error occurred while setting a signal handler.\n", stderr);
-        //return EXIT_FAILURE;
-    }
-    char* buffer_send = readFileInBuffer("/opt/hgs/file.txt", &transfer_size);
-    if ( buffer_send )
-    {
-    	char* buffer_recv = aligned_malloc(ALIGN_SIZE, transfer_size + 1);
-	if ( 0 == strncmp("l2cap", argv[4], strlen("l2cap") ) )
+	if (signal(SIGINT, catch_function) == SIG_ERR)
 	{
-  		long transSize = packageSizeRecv > packageSizeSend ? packageSizeRecv : packageSizeSend;
-     		g_socket = getl2CapSocket(argv[1], transSize);
+		fputs("An error occurred while setting a signal handler.\n", stderr);
+		//return EXIT_FAILURE;
 	}
-	else if ( 0 == strncmp("rfcomm", argv[4], strlen("rfcomm") ) )
+	if (signal(SIGKILL, catch_function) == SIG_ERR)
 	{
-		g_socket = getRfcommSocket(argv[1]);
+		fputs("An error occurred while setting a signal handler.\n", stderr);
+		//return EXIT_FAILURE;
+	}
+	char* buffer_send = readFileInBuffer("/opt/hgs/file.txt", &transfer_size);
+	if ( buffer_send )
+	{
+		char* buffer_recv = aligned_malloc(ALIGN_SIZE, transfer_size + 1);
+		if ( 0 == strncmp("l2cap", argv[4], strlen("l2cap") ) )
+		{
+			long transSize = packageSizeRecv > packageSizeSend ? packageSizeRecv : packageSizeSend;
+			g_socket = getl2CapSocket(argv[1], transSize);
+		}
+		else if ( 0 == strncmp("rfcomm", argv[4], strlen("rfcomm") ) )
+		{
+			g_socket = getRfcommSocket(argv[1]);
+		}
+		else
+		{
+			fprintf(stderr, "No valid protocol\n");
+		}
+		fprintf(stderr, "Using protocol %s\n", argv[4]);
+		// send a message
+		if (0 == g_status)
+		{
+			struct timeval tv;
+			tv.tv_sec = 1;
+			tv.tv_usec = 0;
+			setsockopt(g_socket, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof(tv));
+
+			//for(int packageSize = 512; packageSize < 4096; packageSize++)
+			{
+				//sprintf(filename, "outfile%d", packageSize);
+				sendRecFile(buffer_send, buffer_recv, transfer_size, "outfile", packageSizeSend, packageSizeRecv, argv[4], latency_test);
+			}
+		}
+		else
+		{
+			perror( "Could not connect\n" );
+			fprintf(stderr, "Server address was: %s\n", argv[1]);
+		}
+
+		close(g_socket);
 	}
 	else
 	{
-		fprintf(stderr, "No valid protocol\n");
+		fputs("Could not read /opt/hgs/file.txt\n", stderr);
 	}
-	fprintf(stderr, "Using protocol %s\n", argv[4]);
-	// send a message
-        if (0 == g_status)
-	{
-		struct timeval tv;
-        	tv.tv_sec = 1;
-		tv.tv_usec = 0;
-        	setsockopt(g_socket, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof(tv));
-
-            //for(int packageSize = 512; packageSize < 4096; packageSize++)
-            {
-                //sprintf(filename, "outfile%d", packageSize);
-                sendRecFile(buffer_send, buffer_recv, transfer_size, "outfile", packageSizeSend, packageSizeRecv, argv[4], latency_test);
-            }
-	}
-        else
-        {
-            perror( "Could not connect\n" );
-            fprintf(stderr, "Server address was: %s\n", argv[1]);
-        }
-
-        close(g_socket);
-   }
-   else
-   {
-	   fputs("Could not read /opt/hgs/file.txt\n", stderr);
-   }
 }
 
 char* readFileInBuffer(char* filename, long* psize)
 {
-    char *string = NULL;
-    FILE *f = fopen(filename, "rb");
-    if ( f )
-    {
-        fseek(f, 0, SEEK_END);
-        long fsize = ftell(f);
-        *psize = fsize;
-        fseek(f, 0, SEEK_SET);  /* same as rewind(f); */
+	char *string = NULL;
+	FILE *f = fopen(filename, "rb");
+	if ( f )
+	{
+		fseek(f, 0, SEEK_END);
+		long fsize = ftell(f);
+		*psize = fsize;
+		fseek(f, 0, SEEK_SET);  /* same as rewind(f); */
 
-        string = aligned_malloc(ALIGN_SIZE, fsize + 1);
-        fread(string, 1, fsize, f);
-        fclose(f);
+		string = aligned_malloc(ALIGN_SIZE, fsize + 1);
+		fread(string, 1, fsize, f);
+		fclose(f);
 
-        string[fsize] = 0;
-    }
+		string[fsize] = 0;
+	}
 
-    return string;
+	return string;
 }
--- rfcomm-server.c
+++ rfcomm-server.c
@@ -15,202 +15,202 @@ int fd_sock_rfcomm, fd_sock_l2cap, client;
 
 static void catch_function(int signo)
 {
-    puts("Interactive attention signal caught.");
-    close(client);
-    close(fd_sock_rfcomm);
+	puts("Interactive attention signal caught.");
+	close(client);
+	close(fd_sock_rfcomm);
 
-    exit(1);
+	exit(1);
 }
 
 int getRfcommSocket()
 {
-    char buf[1024] = { 0 };
-    struct sockaddr_rc loc_addr_rfcomm = { 0 } , rem_addr_rfcomm = { 0 } ;
-    unsigned int opt_rfcomm = sizeof (rem_addr_rfcomm);
+	char buf[1024] = { 0 };
+	struct sockaddr_rc loc_addr_rfcomm = { 0 } , rem_addr_rfcomm = { 0 } ;
+	unsigned int opt_rfcomm = sizeof (rem_addr_rfcomm);
 
-    // allocate socket
-    fd_sock_rfcomm = socket (AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+	// allocate socket
+	fd_sock_rfcomm = socket (AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
-    // bind socket to port 1 of the first available bluetooth adapter
-    loc_addr_rfcomm.rc_family = AF_BLUETOOTH ;
-    loc_addr_rfcomm.rc_bdaddr = *BDADDR_ANY ;
-    loc_addr_rfcomm.rc_channel =1;
-    bind(fd_sock_rfcomm, (struct sockaddr *)&loc_addr_rfcomm, sizeof(loc_addr_rfcomm));
+	// bind socket to port 1 of the first available bluetooth adapter
+	loc_addr_rfcomm.rc_family = AF_BLUETOOTH ;
+	loc_addr_rfcomm.rc_bdaddr = *BDADDR_ANY ;
+	loc_addr_rfcomm.rc_channel =1;
+	bind(fd_sock_rfcomm, (struct sockaddr *)&loc_addr_rfcomm, sizeof(loc_addr_rfcomm));
 
-    // put socket into listening mode
-    listen(fd_sock_rfcomm, 1);
+	// put socket into listening mode
+	listen(fd_sock_rfcomm, 1);
 
-    // accept one connection
-    client = accept(fd_sock_rfcomm, (struct sockaddr *)&rem_addr_rfcomm, &opt_rfcomm);
+	// accept one connection
+	client = accept(fd_sock_rfcomm, (struct sockaddr *)&rem_addr_rfcomm, &opt_rfcomm);
 
-    ba2str(&rem_addr_rfcomm.rc_bdaddr, buf);
-    fprintf(stderr, "accepted connection from %s\n", buf);
+	ba2str(&rem_addr_rfcomm.rc_bdaddr, buf);
+	fprintf(stderr, "accepted connection from %s\n", buf);
 
-    return client;
+	return client;
 }
 
 int getl2CapSocket(uint16_t mtu)
 {
-    char buf[1024] = { 0 };
-    struct sockaddr_l2 loc_addr_l2 = { 0 },  rem_addr_l2 = { 0 };
-    unsigned int opt_l2 = sizeof (rem_addr_l2);
+	char buf[1024] = { 0 };
+	struct sockaddr_l2 loc_addr_l2 = { 0 },  rem_addr_l2 = { 0 };
+	unsigned int opt_l2 = sizeof (rem_addr_l2);
 
-    fd_sock_l2cap = socket(AF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP);
-    loc_addr_l2.l2_family = AF_BLUETOOTH;
-    loc_addr_l2.l2_bdaddr = *BDADDR_ANY;
-    loc_addr_l2.l2_psm = htobs(0x1001);
+	fd_sock_l2cap = socket(AF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP);
+	loc_addr_l2.l2_family = AF_BLUETOOTH;
+	loc_addr_l2.l2_bdaddr = *BDADDR_ANY;
+	loc_addr_l2.l2_psm = htobs(0x1001);
 
-    set_l2cap_mtu( fd_sock_l2cap, mtu );
+	set_l2cap_mtu( fd_sock_l2cap, mtu );
 
-    bind(fd_sock_l2cap, (struct sockaddr *)&loc_addr_l2, sizeof(loc_addr_l2));
+	bind(fd_sock_l2cap, (struct sockaddr *)&loc_addr_l2, sizeof(loc_addr_l2));
 
-    // put socket into listening mode
-    listen(fd_sock_l2cap, 1);
+	// put socket into listening mode
+	listen(fd_sock_l2cap, 1);
 
-    // accept one connection
-    client = accept(fd_sock_l2cap, (struct sockaddr *)&rem_addr_l2, &opt_l2);
-    ba2str( &rem_addr_l2.l2_bdaddr, buf );
-    fprintf(stderr, "accepted connection from %s\n", buf);
+	// accept one connection
+	client = accept(fd_sock_l2cap, (struct sockaddr *)&rem_addr_l2, &opt_l2);
+	ba2str( &rem_addr_l2.l2_bdaddr, buf );
+	fprintf(stderr, "accepted connection from %s\n", buf);
 
-    return client;
+	return client;
 }
 int main (int argc, char** argv)
 {
-    char *buffer_recv;
-    char *buffer_send;
-    static const long bufsize = BUF_SIZE; //sizeof (buf)/sizeof(char);
-    int status;
+	char *buffer_recv;
+	char *buffer_send;
+	static const long bufsize = BUF_SIZE; //sizeof (buf)/sizeof(char);
+	int status;
 
-    if ( argc < 4 )
-    {
-        printf("Usage: client <package_size_recv> <package_size_send> <protocol>");
-        return 1;
-    }
-    char* p;
-    long packageSizeRecv = strtol(argv[1], &p, 10);
-    long packageSizeSend = strtol(argv[2], &p, 10);
+	if ( argc < 4 )
+	{
+		printf("Usage: client <package_size_recv> <package_size_send> <protocol>");
+		return 1;
+	}
+	char* p;
+	long packageSizeRecv = strtol(argv[1], &p, 10);
+	long packageSizeSend = strtol(argv[2], &p, 10);
 
-    buffer_recv = aligned_malloc(ALIGN_SIZE, packageSizeRecv);
-    buffer_send = aligned_malloc(ALIGN_SIZE, packageSizeSend);
+	buffer_recv = aligned_malloc(ALIGN_SIZE, packageSizeRecv);
+	buffer_send = aligned_malloc(ALIGN_SIZE, packageSizeSend);
 
-    if (signal(SIGINT, catch_function) == SIG_ERR)
-    {
-        fputs("An error occurred while setting a signal handler.\n", stderr);
-        //return EXIT_FAILURE;
-    }
-    if (signal(SIGKILL, catch_function) == SIG_ERR)
-    {
-        fputs("An error occurred while setting a signal handler.\n", stderr);
-        //return EXIT_FAILURE;
-    }
+	if (signal(SIGINT, catch_function) == SIG_ERR)
+	{
+		fputs("An error occurred while setting a signal handler.\n", stderr);
+		//return EXIT_FAILURE;
+	}
+	if (signal(SIGKILL, catch_function) == SIG_ERR)
+	{
+		fputs("An error occurred while setting a signal handler.\n", stderr);
+		//return EXIT_FAILURE;
+	}
 
-    init_bt();
-    //memset ( buffer_recv, 0, sizeof ( buffer_recv));
+	init_bt();
+	//memset ( buffer_recv, 0, sizeof ( buffer_recv));
 
-    /*
-    remove("out.bin");
-    FILE* pFile;
-    pFile = fopen("out.bin", "ab");
-    if( pFile )
-    { 
-	    fprintf(stderr, "Opened file\n");
-    }
-    else
-    {
-	    fprintf(stderr, "Failed to open file\n");
-    }
-    */
-    if ( 0 == strncmp("l2cap", argv[3], strlen("l2cap") ) )
-    {
-	long transSize = packageSizeRecv > packageSizeSend ? packageSizeRecv : packageSizeSend;
-	fprintf(stderr, "Using l2cap mtu %ld\n", transSize);
-	client = getl2CapSocket(transSize);
-    }
-    else if ( 0 == strncmp("rfcomm", argv[3], strlen("rfcomm") ) )
-    {
-    	client = getRfcommSocket();
-    }
-    else
-    {
-	fprintf(stderr, "No valid protocol\n");
-    }
-    fprintf(stderr, "Using protocol %s\n", argv[3]);
-
-
-    long transfer_size = 0;
-    long bytes_read = 0;
-    long write_length = 0;
-    long read_length = 0;
-    long read_index = 0;
-    long write_index = 0;
-
-    fd_set read_sd;
-    FD_ZERO(&read_sd);
-    FD_SET(client, &read_sd);
-    //int packageSize = 512;
-    fprintf(stderr, "Working with paketSizeFrom%ld, packetSizeTo\n", packageSizeRecv, packageSizeSend);
-    while(1)
-    {
-
-        bytes_read = 0;
-        int bytes_read_temp = 0;
-        read_length = packageSizeRecv; //( transfer_size - read_index) > packageSize ? packageSize : transfer_size - read_index;
-
-        while ( bytes_read < read_length )
-        {
-            //fprintf(stderr, "waiting for  %ld bytes \n", read_length - bytes_read);
-            //bytes_read_temp = recv(client, buffer_recv + read_index, read_length, 0);
-
-            fd_set rsd = read_sd;
-            int sel = select(client + 1, &rsd, 0, 0, 0);
-            if (sel > 0)
-            {
-                bytes_read_temp = recv(client, buffer_recv, read_length, 0);
-                if ( 0 == bytes_read_temp)
-                {
-                    fprintf(stderr, "Client disconnected\n");
-                    return 0;
-                }
-                //fprintf(stderr, "bytes_read %ld\n", bytes_read_temp);
-                if( bytes_read_temp <= 0 )
-                {
-                    fprintf(stderr, "TIMEOUT\n");
-                    if ( read_index == 0 )
-                    {
-                        return bytes_read_temp;
-                    }
-			return 0;
-                    break;
-                }
-
-                read_index += bytes_read_temp;
-                bytes_read += bytes_read_temp;
-            }
-            else if (sel < 0)
-            {
-                // grave error occurred.
-                break;
-            }
-        }
-        if ( packageSizeSend > 0)
-        {
-            //fwrite(buffer_recv, sizeof(char), bytes_read, pFile);
-            //printf(stderr, "sending %ld bytes\n", bytes_read);
-            status = send(client, buffer_send, packageSizeSend, 0);
-            //fprintf(stderr, "sent %ld bytes\n", bytes_read);
-        }
-        else
-        {
-            //fprintf(stderr, "no bytes received \n");
-            //perror("Error receving bytes:");
-        }
-    }
+	/*
+	   remove("out.bin");
+	   FILE* pFile;
+	   pFile = fopen("out.bin", "ab");
+	   if( pFile )
+	   { 
+	   fprintf(stderr, "Opened file\n");
+	   }
+	   else
+	   {
+	   fprintf(stderr, "Failed to open file\n");
+	   }
+	 */
+	if ( 0 == strncmp("l2cap", argv[3], strlen("l2cap") ) )
+	{
+		long transSize = packageSizeRecv > packageSizeSend ? packageSizeRecv : packageSizeSend;
+		fprintf(stderr, "Using l2cap mtu %ld\n", transSize);
+		client = getl2CapSocket(transSize);
+	}
+	else if ( 0 == strncmp("rfcomm", argv[3], strlen("rfcomm") ) )
+	{
+		client = getRfcommSocket();
+	}
+	else
+	{
+		fprintf(stderr, "No valid protocol\n");
+	}
+	fprintf(stderr, "Using protocol %s\n", argv[3]);
 
 
-    // close connection
-    aligned_free(buffer_recv);
-    aligned_free(buffer_send);
-    close(client);
-    close(fd_sock_rfcomm);
-    return 0;
+	long transfer_size = 0;
+	long bytes_read = 0;
+	long write_length = 0;
+	long read_length = 0;
+	long read_index = 0;
+	long write_index = 0;
+
+	fd_set read_sd;
+	FD_ZERO(&read_sd);
+	FD_SET(client, &read_sd);
+	//int packageSize = 512;
+	fprintf(stderr, "Working with paketSizeFrom%ld, packetSizeTo\n", packageSizeRecv, packageSizeSend);
+	while(1)
+	{
+
+		bytes_read = 0;
+		int bytes_read_temp = 0;
+		read_length = packageSizeRecv; //( transfer_size - read_index) > packageSize ? packageSize : transfer_size - read_index;
+
+		while ( bytes_read < read_length )
+		{
+			//fprintf(stderr, "waiting for  %ld bytes \n", read_length - bytes_read);
+			//bytes_read_temp = recv(client, buffer_recv + read_index, read_length, 0);
+
+			fd_set rsd = read_sd;
+			int sel = select(client + 1, &rsd, 0, 0, 0);
+			if (sel > 0)
+			{
+				bytes_read_temp = recv(client, buffer_recv, read_length, 0);
+				if ( 0 == bytes_read_temp)
+				{
+					fprintf(stderr, "Client disconnected\n");
+					return 0;
+				}
+				//fprintf(stderr, "bytes_read %ld\n", bytes_read_temp);
+				if( bytes_read_temp <= 0 )
+				{
+					fprintf(stderr, "TIMEOUT\n");
+					if ( read_index == 0 )
+					{
+						return bytes_read_temp;
+					}
+					return 0;
+					break;
+				}
+
+				read_index += bytes_read_temp;
+				bytes_read += bytes_read_temp;
+			}
+			else if (sel < 0)
+			{
+				// grave error occurred.
+				break;
+			}
+		}
+		if ( packageSizeSend > 0)
+		{
+			//fwrite(buffer_recv, sizeof(char), bytes_read, pFile);
+			//printf(stderr, "sending %ld bytes\n", bytes_read);
+			status = send(client, buffer_send, packageSizeSend, 0);
+			//fprintf(stderr, "sent %ld bytes\n", bytes_read);
+		}
+		else
+		{
+			//fprintf(stderr, "no bytes received \n");
+			//perror("Error receving bytes:");
+		}
+	}
+
+
+	// close connection
+	aligned_free(buffer_recv);
+	aligned_free(buffer_send);
+	close(client);
+	close(fd_sock_rfcomm);
+	return 0;
 }
