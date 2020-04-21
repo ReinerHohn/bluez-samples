b09c4590acdcf179d9a9939e5870ffc0d126882e
Michael Dick <mi-dick@gmx.de>
2020-04-21 16:19:14 +0100

    Add latency test in test script

--- bt-test.sh
+++ bt-test.sh
@@ -8,8 +8,10 @@ IP_CLIENT=$1
 BDADDR_SERVER=$(hciconfig -a | grep "BD Address" | awk ' { print $3 } ' )
 IP_SERVER=$(ip a | grep eth0  | grep inet | awk ' { print $2 } '  | cut -d/ -f1)
 
-PROTOCOL=rfcomm
-#PROTOCOL=l2cap
+#PROTOCOL=rfcomm
+PROTOCOL=l2cap
+
+LATENCY_TEST=1
 
 PROG_CLIENT=/opt/hgs/client
 PROG_SERVER=/opt/hgs/server
@@ -18,7 +20,7 @@ PACK_SIZE_END=8096
 
 ssh-copy-id $IP_CLIENT
 
-i=1600
+i=1
 while [ $i -le $PACK_SIZE_END ];
 do
 	PACK_SIZE_SEND=$i
@@ -26,7 +28,8 @@ do
 
 	#ssh pi@$CLIENT_IP "./server  $PACK_SIZE_SEND $PACK_SIZE_RECV $PROTOCOL" &
 	$PROG_SERVER  $PACK_SIZE_SEND $PACK_SIZE_RECV $PROTOCOL &
-	ssh pi@$IP_CLIENT "$PROG_CLIENT $BDADDR_SERVER $PACK_SIZE_SEND $PACK_SIZE_RECV $PROTOCOL"
+	sleep 2
+	ssh pi@$IP_CLIENT "$PROG_CLIENT $BDADDR_SERVER $PACK_SIZE_SEND $PACK_SIZE_RECV $PROTOCOL $LATENCY_TEST"
 
-	i=$(( $i + 30 ))
+	i=$(( $i * 2 ))
 done
